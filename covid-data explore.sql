28dfaa3cae004c8b506d105e0b84cb70e2db6aeb
katherine-shi <52843167+katherine-shi@users.noreply.github.com>
2022-08-22 17:12:42 -0400

    Add files via upload

--- covid-data explore.sql
+++ covid-data explore.sql
@@ -47,5 +47,51 @@ From covid_19.covid_19_world_cases_deaths_testing
 select *, (RollingPeopleVaccinated/Population)*100 as populationvsvaccinations
 from popvsvac
 
+##queries for tableau project
+##table 1
+select SUM(new_cases) as total_cases, SUM(new_deaths) as total_deaths, SUM(new_deaths)/SUM(new_cases)*100 as DeathPercentage
+From covid_19.covid_19_world_cases_deaths_testing
+#group by date
+order by 1,2;
 
-#
+## table 2
+Select continent, SUM(new_deaths) as total_deaths
+From covid_19.covid_19_world_cases_deaths_testing
+Where location not in ('World', 'European Union', 'International','Upper middle income','High income','Lower middle income','Low income')
+Group by continent
+order by total_deaths desc;
+
+## table 3
+Select continent, SUM(new_deaths) as total_deaths
+From covid_19.covid_19_world_cases_deaths_testing
+Where continent like "%income%"
+Group by continent
+order by total_deaths desc;
+
+#table 4
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From covid_19.covid_19_world_cases_deaths_testing
+Group by Location, Population
+order by PercentPopulationInfected desc;
+
+#table 5
+Select Location, date, population, total_cases, total_deaths
+FROM covid_19.covid_19_world_cases_deaths_testing
+where location not in ("Asia","Africa","Europe","European Union","International","World",'Upper middle income','High income','Lower middle income','Low income','North America','South America','Oceania')
+order by 1,2;
+
+#table 6
+SELECT location,date,population, total_cases, round(total_cases/population,5) as get_covid
+FROM covid_19.covid_19_world_cases_deaths_testing
+order by 1,2;
+
+
+
+
+
+
+
+
+
+
+
